0ef3cf3ec0bd5e042406d80820f5fb74cae922b9
Tim Flood <tflood9@gmail.com>
2018-11-19 14:08:34 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -21,7 +21,7 @@ describe('Arrays', function() {
       expect(window.kittens).toEqual(["Bob", "Milo", "Otis", "Garfield"])
     })
   })
-/*
+
   describe('destructivelyRemoveLastKitten()', function() {
     it('removes the last kitten from the kittens array', function() {
       window.kittens = ['Milo', 'Otis', 'Garfield'];
@@ -30,7 +30,7 @@ describe('Arrays', function() {
       expect(window.kittens).toEqual(["Milo", "Otis"])
     })
   })
-
+/*
   describe('destructivelyRemoveFirstKitten()', function() {
     it('removes the First kitten from the kittens array', function() {
       window.kittens = ['Milo', 'Otis', 'Garfield'];
